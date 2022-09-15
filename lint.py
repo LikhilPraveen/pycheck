1e5e5b051877e50928f6c0ebf74f92cb4ef6580d
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 10:03:34 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -3,9 +3,9 @@ from pylint import lint
 
 THRESHOLD = 5
 
-score = lint.Run(['factorial.py'], exit=False).linter.stats.global_note
+score = lint.Run(['./checking'], exit=False).linter.stats.global_note
 
 if score < THRESHOLD:
     print("Linter failed: Score < threshold value")
-    message = sys.exit(1)
-    print(message)
+    sys.exit(1)
+ 
